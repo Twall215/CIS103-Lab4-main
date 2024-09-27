d72de5f69c5508888f0e1d386e5b14ea05cfce8f
Trey Wallace <contacttreyw@gmail.com>
2024-09-27 10:29:07 -0500

    Original game

--- lab-4.py
+++ lab-4.py
@@ -1,40 +1,69 @@
-import random
 
- 
 
-options = ["Rock", "Paper", "Scissors"]
+def compare():#The game itself
+    import random
+    score = { #Stores the score for the tally
+        "user score" : 0,
+        "bot score" : 0
+    }
 
- 
+    options = ["Rock", "Paper", "Scissors"]
+    user_choice = input("Choose Rock, Paper, or Scissors: ")
+    user_choice = user_choice.strip().title()#Capitalizes the first letter of each word and eliminates spaces
+    computer_choice = random.choice(options)#Randomize choices
+    print("You chose: ", user_choice)
+    print("Computer chose: ", computer_choice)
+    
+    if user_choice == computer_choice:
+        print("It's a tie!")
+        score["user score"] +=1
+        score["bot score"] +=1
+        print(score)
+        
+    elif user_choice == "Rock" and computer_choice == "Scissors":
+        print("You win!")
+        score["user score"] +=1
+        print(score)
+        
+    elif user_choice == "Paper" and computer_choice == "Rock":
+        print("You win!")
+        score["user score"] +=1
+        print(score)
+        
+    elif user_choice == "Scissors" and computer_choice == "Paper":
+        print("You win!")
+        score["user score"] +=1
+        print(score)
+        
+    else:
+        print("Computer wins!")
+        print(score)
+        score["bot score"] +=1      
 
-user_choice = input("Choose Rock, Paper, or Scissors: ")
+#    def multi_rounds():
+#        while score['bot score'] < 2 and score['user score'] < 2:
+#           result = compare()
+#            print(result)
+#            if result == "You win!":
+#                score["user score"] +=1
+#                print(score)
+#            elif result == "Computer winds!":
+#                score["bot score"] +=1
+#                print(score)
+#            elif result == "It's a tie!":
+#                score["bot score"]+=1
+#                score["user score"]+=1
+#                print(score)
+#        if score['bot score'] == 2 and score['user score'] < 2:
+#            print("Checkmate! Bot Wins")
+#            print(score)
+#            
+#        elif score['user score'] == 2 and score['bot score'] < 2:
+#            print("Checkmate! You Win")
+#            print(score)
+       
+if __name__ == "__main__":
+    compare()    
+    
 
-computer_choice = random.choice(options)
-
- 
-
-print("You chose: ", user_choice)
-
-print("Computer chose: ", computer_choice)
-
- 
-
-if user_choice == computer_choice:
-
-    print("It's a tie!")
-
-elif user_choice == "Rock" and computer_choice == "Scissors":
-
-    print("You win!")
-
-elif user_choice == "Paper" and computer_choice == "Rock":
-
-    print("You win!")
-
-elif user_choice == "Scissors" and computer_choice == "Paper":
-
-    print("You win!")
-
-else:
-
-    print("Computer wins!")
 
