7dba7de88cd8d6f81e281c9953b034588f034af2
Trey Wallace <contacttreyw@gmail.com>
2024-09-27 14:13:34 -0500

    Final Product

--- lab-4.py
+++ lab-4.py
@@ -1,69 +1,52 @@
+import random
 
-
-def compare():#The game itself
-    import random
-    score = { #Stores the score for the tally
-        "user score" : 0,
-        "bot score" : 0
+def compare():
+    score = {#tallys the score
+        "user score": 0,
+        "bot score": 0
     }
+    round = 1 #starts off the round counter
+    total_rounds = 3 #Best 2 out of 3
 
     options = ["Rock", "Paper", "Scissors"]
-    user_choice = input("Choose Rock, Paper, or Scissors: ")
-    user_choice = user_choice.strip().title()#Capitalizes the first letter of each word and eliminates spaces
-    computer_choice = random.choice(options)#Randomize choices
-    print("You chose: ", user_choice)
-    print("Computer chose: ", computer_choice)
-    
-    if user_choice == computer_choice:
-        print("It's a tie!")
-        score["user score"] +=1
-        score["bot score"] +=1
-        print(score)
+
+    while round <= total_rounds and (score["bot score"] < 2 and score["user score"] < 2):
+        #Func counts the rounds and also the scores per round
+        user_choice = input("Choose Rock, Paper, or Scissors: ").strip().title()
+        computer_choice = random.choice(options)
+        print("You chose:", user_choice)
+        print("Computer chose:", computer_choice)
+
+        if user_choice == computer_choice:
+            print (f"Round {round}")
+            print("It's a tie!")
+            score["user score"] += 1 #Score counter
+            score["bot score"] += 1
+            print(score)
         
-    elif user_choice == "Rock" and computer_choice == "Scissors":
-        print("You win!")
-        score["user score"] +=1
-        print(score)
-        
-    elif user_choice == "Paper" and computer_choice == "Rock":
-        print("You win!")
-        score["user score"] +=1
-        print(score)
-        
-    elif user_choice == "Scissors" and computer_choice == "Paper":
-        print("You win!")
-        score["user score"] +=1
-        print(score)
-        
+        elif (user_choice == "Rock" and computer_choice == "Scissors") or (user_choice == "Paper" and computer_choice == "Rock") or (user_choice == "Scissors" and computer_choice == "Paper"):
+            print (f"Round {round}")
+            print("You win!")
+            score["user score"] += 1
+            print(score)
+            
+        else:
+            print (f"Round {round}")
+            print("Computer wins!")
+            score["bot score"] += 1
+            print(score)
+
+        round += 1 #Round counter
+
+#this is the winning logic. If they tie 3 times the Func will auto restart until a winner is decided
+    if score['bot score'] == 2 and score['user score'] < 2:
+        print("Checkmate! Bot Wins")
+    elif score['user score'] == 2 and score['bot score'] < 2:
+        print("Checkmate! You Win")
     else:
-        print("Computer wins!")
-        print(score)
-        score["bot score"] +=1      
-    
-    
-        while score["bot score"] < 2 and score["user score"] < 2:
-           result = compare()
-           print(result)
-           
-        if result == "You win!":
-                score["user score"] +=1
-                print(score)
-        elif result == "Computer wins!":
-            score["bot score"] +=1
-            print(score)
-        elif result == "It's a tie!":
-            score["bot score"]+=1
-            score["user score"]+=1
-            print(score)
-        if score["bot score"] == 2 and score["user score"] < 2:
-            print("Checkmate! Bot Wins")
-            print(score)    
-        elif score["user score"] == 2 and score["bot score"] < 2:
-            print("Checkmate! You Win")
-            print(score)
-       
-if __name__ == "__main__":
         compare()
-    
+
+if __name__ == "__main__":
+    compare()
 
 
